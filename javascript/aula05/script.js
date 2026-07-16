eb0b798c562d93217561d231ac5d63b1fa2360fc
wllysses <wllyssestavares@gmail.com>
2026-07-16 20:08:29 -0300

    feat: aula05 javascript

---
+++ javascript/aula05/script.js
@@ -0,0 +1,17 @@
+// APROVAÇÃO - 7
+// RECUPERAÇÃO - 4 e 6.9
+// REPROVADO - abaixo de 4
+
+let mediaAluno;
+
+// SINTAXE
+
+//SE
+if (mediaAluno >= 6) {
+  // EXECUTA ESTE BLOCO
+  document.write("APROVADO!");
+} else {
+  // SENÃO
+  // EXECUTA ESTE BLOCO
+  document.write("REPROVADO!");
+}
---
+++ javascript/calculadora-media-escolar/script.js
@@ -0,0 +1,10 @@
+// ENTRADA DE DADOS
+const nomeEstudante = prompt("Como você se chama?");
+
+const nota1Estudante = Number(prompt("Digite a primeira nota: "));
+
+// PROCESSAMENTO
+// CALCULO DA MÉDIA
+
+// SAÍDA DOS DADOS
+alert(`Olá, ${nomeEstudante}.`);
