949af2ae80b2e27d2e47be21c291154b0fc08bd1
wllysses <wllyssestavares@gmail.com>
2026-09-09 15:10:30 -0300

    add aula 15-dim

---
+++ javascript/aula15-dom/script.js
@@ -0,0 +1,9 @@
+const texto = document.querySelector("p");
+
+function adicionarEstilo() {
+  texto.classList.add("paragrafo");
+}
+
+function removerEstilo() {
+  texto.classList.remove("paragrafo");
+}
