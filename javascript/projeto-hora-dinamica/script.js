0884ba667f324af0fc1529cc4779f62e4e57eefc
wllysses <wllyssestavares@gmail.com>
2026-09-16 19:56:46 -0300

    atualizacao

---
+++ javascript/projeto-hora-dinamica/script.js
@@ -0,0 +1,7 @@
+const horaAtual = new Date().getHours();
+
+const body = document.body;
+const saudacao = document.querySelector("h3");
+const imagem = document.getElementById("fotoPaisagem");
+const periodo = document.getElementById("periodo");
+const frase = document.getElementById("frase");
