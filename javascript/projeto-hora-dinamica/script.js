dc1460524277ae4c155916cbca53c66a5d9f7c96
wllysses <wllyssestavares@gmail.com>
2026-09-16 20:13:03 -0300

    atualizando

--- javascript/projeto-hora-dinamica/script.js
+++ javascript/projeto-hora-dinamica/script.js
@@ -5,3 +5,19 @@ const saudacao = document.querySelector("h3");
 const imagem = document.getElementById("fotoPaisagem");
 const periodo = document.getElementById("periodo");
 const frase = document.getElementById("frase");
+
+saudacao.textContent = `Olá. Agora são ${horaAtual} horas.`;
+
+if (horaAtual >= 0 && horaAtual < 12) {
+  body.style.backgroundColor = "#e2cd9f";
+  periodo.textContent = "Manhã";
+  frase.textContent = "Começando o dia com energia.";
+} else if (horaAtual >= 12 && horaAtual < 18) {
+  body.style.backgroundColor = "#b9846f";
+  periodo.textContent = "Tarde";
+  frase.textContent = "Um bom café para não dormir.";
+} else {
+  body.style.backgroundColor = "#515154";
+  periodo.textContent = "Noite";
+  frase.textContent = "Que lindas estão as estrelas.";
+}
