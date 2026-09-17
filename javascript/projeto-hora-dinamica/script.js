1479d876c1b2a4e390754129992f96e1cbf77db9
wllysses <wllyssestavares@gmail.com>
2026-09-17 19:35:17 -0300

    finalizando o projeto

--- javascript/projeto-hora-dinamica/script.js
+++ javascript/projeto-hora-dinamica/script.js
@@ -12,12 +12,15 @@ if (horaAtual >= 0 && horaAtual < 12) {
   body.style.backgroundColor = "#e2cd9f";
   periodo.textContent = "Manhã";
   frase.textContent = "Começando o dia com energia.";
+  imagem.src = "foto-manha.webp";
 } else if (horaAtual >= 12 && horaAtual < 18) {
   body.style.backgroundColor = "#b9846f";
   periodo.textContent = "Tarde";
   frase.textContent = "Um bom café para não dormir.";
+  imagem.src = "foto-tarde.jpg";
 } else {
   body.style.backgroundColor = "#515154";
   periodo.textContent = "Noite";
   frase.textContent = "Que lindas estão as estrelas.";
+  imagem.src = "foto-noite.avif";
 }
