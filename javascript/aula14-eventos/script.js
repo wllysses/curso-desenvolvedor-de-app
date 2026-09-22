743df2365ebb73fa955715f5ccca469c02f8cd48
wllysses <wllyssestavares@gmail.com>
2026-09-22 19:46:12 -0300

    adicionando o evento de submit

--- javascript/aula14-eventos/script.js
+++ javascript/aula14-eventos/script.js
@@ -12,3 +12,15 @@ const nomeUsuario = document.querySelector("input");
 nomeUsuario.addEventListener("input", function () {
   console.log(nomeUsuario.value);
 });
+
+// EVENTO CHANGE
+const select = document.querySelector("select");
+
+select.addEventListener("change", function (evento) {
+  console.log(evento.target.value);
+});
+
+//EVENTO SUBMIT
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+const form = document.querySelector("form");
